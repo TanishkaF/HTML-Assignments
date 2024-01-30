f9e260d447dbc96bcc2497f937edd457742caae4
TanishkaF <tanishkaf@mindfiresolutions.com>
2024-01-30 10:17:56 +0530

    added generic function

---
+++ SchoolEntity/Extension/Helper.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System;
+
+namespace SchoolEntity.Extension
+{
+    public static class Helper
+    {
+        public static List<T> ConvertDataTableToList<T>(this DataTable dt) where T : class, new()
+        {
+            try
+            {
+                List<T> list = new List<T>();
+
+                foreach (var row in dt.AsEnumerable())
+                {
+                    T obj = new T();
+
+                    foreach (var prop in obj.GetType().GetProperties())
+                    {
+                        try
+                        {
+                            PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
+
+                            // Handle nullable types
+                            if (propertyInfo.PropertyType.IsGenericType && propertyInfo.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                            {
+                                if (row[prop.Name] == DBNull.Value)
+                                {
+                                    propertyInfo.SetValue(obj, null, null);
+                                }
+                                else
+                                {
+                                    propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], Nullable.GetUnderlyingType(propertyInfo.PropertyType)), null);
+                                }
+                            }
+                            else
+                            {
+                                propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
+                            }
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
+  
---
+++ SchoolEntity/Extension/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+
+namespace SchoolEntity.Extension
+{
+    public class Program
+    {
+        public static void AddData(Exception inputData)
+        {
+            //string fileName = @"C:\Users\tanishkaf\Desktop\dotnetprac\A2\logFile.txt";
+            //string logFilePath = ConfigurationManager.AppSettings["LogFilePath"];
+
+            string logFolderPath = ConfigurationManager.AppSettings["LogFolderPath"];
+
+            string folderPath = Path.Combine(logFolderPath, DateTime.Now.ToString("yyyy-MM-dd"));
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+
+            }
+            string logFileName = $"logFile_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.txt";
+            string fullLogFilePath = Path.Combine(folderPath, logFileName);
+
+
+            string logMessage = $"Time: {DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")}{Environment.NewLine}" +
+                                "-----------------------------------------------------------" +
+                                $"{Environment.NewLine}Exception Details:{Environment.NewLine}" +
+                                $"Message: {inputData.Message}{Environment.NewLine}" +
+                                $"StackTrace: {inputData.StackTrace}{Environment.NewLine}" +
+                                $"Source: {inputData.Source}{Environment.NewLine}" +
+                                $"TargetSite: {inputData.TargetSite?.ToString()}{Environment.NewLine}" +
+                                "-----------------------------------------------------------" +
+                                $"{Environment.NewLine}";
+
+            using (StreamWriter writer = new StreamWriter(fullLogFilePath, true))
+            {
+                writer.WriteLine(logMessage);
+            }
+        }
+    }
+}
